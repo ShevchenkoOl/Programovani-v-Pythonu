ed6e54379b00a53815f0e2d5a7b75dc38acfc68a
Oleksii Shevchenko <105150555+ShevchenkoOl@users.noreply.github.com>
2025-01-30 15:37:58 +0100

    Update basis.py

--- basis.py
+++ basis.py
@@ -5,3 +5,8 @@ print(type(b))      # <class 'list'>
 print(type('alex')) # <class 'str'>
 print(type(3))      # <class 'int'>
 print(type(3.5))    # <class 'float'>
+
+
+text = 'Here is some random text'
+print(text.find('h'))  # -1
+print(text.find('H'))  # 0 - toto je indexové číslo, kde se dané písmeno nachází
