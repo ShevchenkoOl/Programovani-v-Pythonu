58b395e4b828da58954517b34c00cea1ad7d4856
Oleksii Shevchenko <105150555+ShevchenkoOl@users.noreply.github.com>
2025-01-30 12:40:43 +0100

    Create basis.py

---
+++ basis.py
@@ -0,0 +1,7 @@
+a = True
+b = []
+print(type(a))      # <class 'bool'>
+print(type(b))      # <class 'list'>
+print(type('alex')) # <class 'str'>
+print(type(3))      # <class 'int'>
+print(type(3.5))    # <class 'float'>
