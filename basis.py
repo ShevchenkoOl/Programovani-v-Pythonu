19ce5f9634be66e8efb1c9df3c09b0cfdcc6eb8b
Oleksii Shevchenko <105150555+ShevchenkoOl@users.noreply.github.com>
2025-02-04 13:26:51 +0100

    Update basis.py

--- basis.py
+++ basis.py
@@ -1,26 +1,40 @@
-a = True
-b = []
-print(type(a))      # <class 'bool'>
-print(type(b))      # <class 'list'>
-print(type('alex')) # <class 'str'>
-print(type(3))      # <class 'int'>
-print(type(3.5))    # <class 'float'>
+# a = True
+# b = []
+# print(type(a))      # <class 'bool'>
+# print(type(b))      # <class 'list'>
+# print(type('alex')) # <class 'str'>
+# print(type(3))      # <class 'int'>
+# print(type(3.5))    # <class 'float'>
 
 
-text = 'Here is some random text'
-print(text.find('h'))  # -1
-print(text.find('H'))  # 0 - toto je indexové číslo, kde se dané písmeno nachází
+# text = 'Here is some random text'
+# print(text.find('h'))  # -1
+# print(text.find('H'))  # 0 - toto je indexové číslo, kde se dané písmeno nachází
 
-radky = [
-    [2001, 7.8],
-    [2002, 8.7],
-    [2003, 8.2],
-    [2004, 7.8],
-    [2005, 7.7],
-    [2006, 8.2],
-    [2007, 9.1],
-    [2008, 8.9],
-    [2009, 8.4],
-    [2010, 7.2]
-]
-print(radky[-1]) # [2010, 7.2] - posledni prvek seznamu
+# radky = [
+#     [2001, 7.8],
+#     [2002, 8.7],
+#     [2003, 8.2],
+#     [2004, 7.8],
+#     [2005, 7.7],
+#     [2006, 8.2],
+#     [2007, 9.1],
+#     [2008, 8.9],
+#     [2009, 8.4],
+#     [2010, 7.2]
+# ]
+# print(radky[-1]) # [2010, 7.2] - posledni prvek seznamu
+
+
+
+#--------------Vlastní funkce
+# def greet_user():  # def - definici funkce, nazev_funkce(parametry)
+#     print("Ahoj!") # tělo funkce 
+
+# greet_user()       # volani funkce
+#                    # po vyvolání funkce se spustí print()  a zobrazí se konzole 'Ahoj'
+# def sum_two_numbers(a, b):
+#     return a + b
+
+# suma_cisel = sum_two_numbers(3, 5)
+# print(suma_cisel)   # 8
